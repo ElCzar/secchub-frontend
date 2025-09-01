f0561b59e6e60f21dc5bbb9395256870a923bbea
pao407 <paoberu407@gmail.com>
2025-08-31 23:26:47 -0500

    style: Implentar el diseño del login

--- src/app/features/auth/pages/login-page/login-page.component.ts
+++ src/app/features/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule, LoginFormComponent],
+  imports: [ LoginFormComponent],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
--- src/app/features/auth/services/auth/auth.service.ts
+++ src/app/features/auth/services/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
 import { AuthTokenDto, LoginDto } from '../../models/auth.models';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
