00a8f6f33d4bb92ca0a2b52b0fbac840f0a0b452
pao407 <paoberu407@gmail.com>
2025-09-10 23:10:56 -0500

    chore:Agreglar Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,15 @@ import { Routes } from '@angular/router';
 
 import { ConfirmacionPageComponent } from './features/confirmacion-docentes/pages/confirmacion-page/confirmacion-page.component';
 import { ProgramasPageComponent } from './features/programas/pages/programas-page/programas-page.component';
+import { LoginPageComponent } from './features/auth/pages/login-page/login-page.component';
 
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./features/auth/pages/login-page/login-page.component')
-        .then(m => m.LoginPageComponent),
-  },
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: '**', redirectTo: 'login' }
-
+  {path: '',component: LoginPageComponent },
+  
   { path: 'FormularioProgramas', component: ProgramasPageComponent },
 
-    { path: 'FormularioConfirmacionDocentes', component: ConfirmacionPageComponent }
+  { path: 'FormularioConfirmacionDocentes', component: ConfirmacionPageComponent }
    
 ];
 
