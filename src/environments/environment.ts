76546fd544ebf9a7cd55c686ee0c97b2a7f677c5
Cesar Andres Olarte <cesarandresolarte@gmail.com>
2025-10-23 17:24:21 -0500

    feat(deployment): add configuration for deployment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
 export const environment = {
-    //production: false,
+    production: false,
     apiUrl: 'http://localhost:8080',
-    // Usar la URL real de local según backend (con context-path /v1)
-    // Paramétricos locales expuestos en /v1/parametric
     parametricBaseUrl: 'http://localhost:8080/v1',
-    // Fallback solo si no se define parametricBaseUrl
 };
